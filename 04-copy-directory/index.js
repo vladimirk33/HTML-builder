8b5c0743883c72c5b5aec17724c793545104583c
Vladimir <mr.katenin@mail.ru>
2022-05-20 19:29:15 +0300

    feat: add copy directory algorithm

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
+const fs = require('fs');
+const fsPr = require('fs/promises');
+const path = require('path');
 
+const pathFrom = path.join(__dirname, 'files');
+const pathTo = path.join(__dirname, 'files-copy');
+
+fs.access(pathTo, (error) => {
+  if (error) {
+    fsPr.mkdir(pathTo);
+  }
+});
+
+async function copyDirectory(fromPath, toPath) {
+
+  await fsPr.rm(toPath, { force: true, recursive: true });
+  await fsPr.mkdir(toPath, { recursive: true });
+
+  const filesNameArray = await fsPr.readdir(fromPath, { withFileTypes: true });
+
+  for (let item of filesNameArray) {
+    const currentItemPath = path.join(fromPath, item.name);
+    const copyItemPath = path.join(toPath, item.name);
+
+    if (item.isFile()) {
+      await fsPr.copyFile(currentItemPath, copyItemPath);
+    } else if (item.isDirectory()) {
+      await fsPr.mkdir(copyItemPath, { recursive: true });
+      await copyDirectory(currentItemPath, copyItemPath);
+    }
+  }
+}
+
+copyDirectory(pathFrom, pathTo);
